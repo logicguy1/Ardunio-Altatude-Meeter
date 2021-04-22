3e714087d5bdce18503eace024d10c8fff6ccb6e
logicguy1 <56993729+logicguy1@users.noreply.github.com>
2021-04-22 11:30:02 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -6,33 +6,29 @@ void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-  ; // wait for serial port to connect. Needed for native USB port only
+    while (1); // wait for serial port to connect. Needed for native USB port only
   } 
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(10)) {
+}
+
+void loop() {
+  write_to_file("Test");
+
+}
+
+void write_to_file(String data) {
+  if (!SD.begin(10)) { // Initialize the sd card 
     Serial.println("initialization failed!");
     while (1);
   }
-  Serial.println("initialization done.");
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("This is a test file :)");
-    myFile.println("testing 1, 2, 3.");
-    for (int i = 0; i < 20; i++) {
-      myFile.println(i);
-    }
-  // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-  // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+
+  myFile = SD.open("data.txt", FILE_WRITE); // Open te file 
+  
+  if (!myFile) {
+    // if the file didn't open, print an error:
+    Serial.println("error opening data.txt");
+    while (1);
   }
-}
-void loop() {
-// nothing happens after setup
+
+  myFile.println(data);
+  myFile.close();
 }
