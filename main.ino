4bf177d03bdf21b7d056278923aefa2f4a13cfc9
logicguy1 <56993729+logicguy1@users.noreply.github.com>
2021-04-23 09:55:52 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -9,32 +9,16 @@
  *     |    |    |    |    |    |  
  *    GND   5V  D12  D11  D13  D10
  *
+ * Pmod NAV
+ * 
+ * Circuit:
+ * 
+ *  |                              |
+ *  | 3V3  GND  SCK       SDI      |
+ *  |--|----|----|----|----|----|--| 
+ *     |    |    |    |    |    |  
+ *    3V3  GND   A5        A4  
  ****************************************************************************/
-
-/************************************************************************
-
-  Test of Pmod NAV (Based on Jim Lindblom's program)
-
-*************************************************************************
-
-  Description: Pmod_NAV
-  All data (accelerometer, gyroscope, magnetometer) are displayed
-  In the serial monitor
-
-  Material
-  1. Arduino Uno
-  2. Pmod NAV (dowload library
-  https://github.com/sparkfun/SparkFun_LSM9DS1_Arduino_Library )
-  Licence Beerware
-
-  Wiring
-  Module<--------------------> Arduino
-  J1 pin 6 (3V3)     to        3V3
-  J1 pin 5 (GND)     to        GND
-  J1 pin 4 (SCK)     to        A5 (SCL)
-  J1 pin 2 (SDI)     to        A4 (SDA)
-
-************************************************************************/
 
 // The earth's magnetic field varies according to its location.
 // Add or subtract a constant to get the right value
@@ -42,12 +26,7 @@
 // http://www.ngdc.noaa.gov/geomag-web/#declination
 
 #define DECLINATION -3.73 // declination (in degrees) in Cluj-Napoca (Romania).
-
-/************************************************************************/
-
 #define PRINT_CALCULATED  //print calculated values
-//#define PRINT_RAW       //print raw data
-
 
 // Call of libraries
 #include <Wire.h>
@@ -56,9 +35,6 @@
 #include <SPI.h>
 #include <SD.h>
 #include <LPS25HBSensor.h>
-
-File myFile;
-
 
 #if defined(ARDUINO_SAM_DUE)
 #define DEV_I2C Wire1   //Define which I2C bus is used. Wire1 for the Arduino Due
@@ -76,6 +52,7 @@ LPS25HBSensor PressTemp(&DEV_I2C);
 
 
 LSM9DS1 imu; // Creation of the object
+File myFile;
 
 int readingNr = 0;
 int ONEplay = 0;
@@ -84,29 +61,8 @@ unsigned long myTime;
 
 void setup(void)
 {
-  //Koordinator 1.
   Serial.begin(9600); // initialization of serial communication
-
-    
-  /*if(ONEplay == 0){
-
-  Serial.println("Pres[hPa]: ???  /   Temp[C]: ???");
-  Serial.println("G: ?.??  /  ?.??  /  ?.?? deg/s");
-  Serial.println("A: ?.??  /  ?.??  /  ?.?? g");
-  Serial.println("M: ?.??  /  ?.??  /  ?.?? gauss");
-  Serial.println("Pitch, Roll:  ??.??  /  ?.??[grader]");
-  Serial.println("Heading: ???.??[grader]");
-  Serial.println("  ");
-
-  ONEplay = 1;
-  delay(1000);
   
-  }*/
-  
- // pinMode(13, OUTPUT);
-
-  // Initialize serial for output
-
   // Initialize I2C bus.
   DEV_I2C.begin();
 
@@ -131,13 +87,6 @@ void loop()
 {
 
   myTime = millis();
-  /*Led blinking.
-  digitalWrite(13, HIGH);
-  delay(25); //Normal 250ms
-  digitalWrite(13, LOW);
-  delay(25); //Normal 250ms
-  */
-  
 
   //measure 1.
   if ( imu.gyroAvailable() )
@@ -155,66 +104,8 @@ void loop()
 
   //display data
  
- 
-
   calc(imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
     
-}
-
-/*display gyroscope data
-void printGyro()
-{
-#ifdef PRINT_CALCULATED
-  Serial.print(imu.calcGyro(imu.gx), 2);  //calculate angular velocity
-    Serial.print(",");
-  Serial.print(imu.calcGyro(imu.gy), 2);
-    Serial.print(",");
-  Serial.print(imu.calcGyro(imu.gz), 2);             //measured in deg/s
-#elif defined PRINT_RAW
-  Serial.print(imu.gx);                   //or display raw data
-    Serial.print(",");
-  Serial.print(imu.gy);
-    Serial.print(",");
-  Serial.println(imu.gz);
-#endif
-}*/
-
-
-
-//display accelerometer data
-void printAccel()
-{
-#ifdef PRINT_CALCULATED
-  Serial.print(imu.calcAccel(imu.ax), 2);   //calculate acceleration
-    Serial.print(",");
-  Serial.print(imu.calcAccel(imu.ay), 2);
-    Serial.print(",");
-  Serial.print(imu.calcAccel(imu.az), 2);                 //measured in g
-#elif defined PRINT_RAW
-  Serial.print(imu.ax);                   //or display raw data
-    Serial.print(",");
-  Serial.print(imu.ay);
-    Serial.print(",");
-  Serial.println(imu.az);
-#endif
-}
-
-//display magnetometer data
-void printMag()
-{
-#ifdef PRINT_CALCULATED
-  Serial.print(imu.calcMag(imu.mx), 2);   //calculate magnetic field components
-    Serial.print(",");
-  Serial.print(imu.calcMag(imu.my), 2);
-    Serial.print(",");
-  Serial.print(imu.calcMag(imu.mz), 2);                //measured in gauss
-#elif defined PRINT_RAW
-  Serial.print(imu.mx);                   //or display raw data
-    Serial.print(",");
-  Serial.print(imu.my);
-    Serial.print(",");
-  Serial.print(imu.mz);
-#endif
 }
 
 //display additional calculated values
